5ada094f339a668aa8836dd7bfa3a04ec21e1950
aldo portillo <94483081+aldoportillo@users.noreply.github.com>
2025-04-07 16:08:47 -0500

    Create comments scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_07_210625) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_07_210718) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "post_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"

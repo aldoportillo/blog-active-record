cf8dcb9afc7f352339564c6ec398cf3762c7c71c
aldo portillo <94483081+aldoportillo@users.noreply.github.com>
2025-04-07 12:05:50 -0500

    create posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_07_163248) do
+ActiveRecord::Schema[8.0].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.boolean "published"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 end
